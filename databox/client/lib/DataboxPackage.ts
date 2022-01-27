94e241f015ec9caa4cd3e19fd6a514d2b2e93ada
Blake Byrnes <blakebyrnes@gmail.com>
2022-01-27 16:40:38 -0500

    fix(databox): server config

--- databox/client/lib/DataboxPackage.ts
+++ databox/client/lib/DataboxPackage.ts
@@ -1,5 +1,6 @@
 import IDataboxRunOptions from '@ulixee/databox-interfaces/IDataboxRunOptions';
 import IDataboxPackage from '@ulixee/databox-interfaces/IDataboxPackage';
+import UlixeeConfig from '@ulixee/commons/config';
 import readCommandLineArgs from './utils/readCommandLineArgs';
 import Interactor from './Interactor';
 import Extractor from './Extractor';
@@ -10,12 +11,23 @@ export default class DataboxPackage implements IDataboxPackage {
   #components: IComponents;
 
   constructor(components: IInteractFn | IComponents) {
-    this.#components = (typeof components === 'function') ? {
-      interact: components as IInteractFn,
-    } : { ...components };
+    this.#components =
+      typeof components === 'function'
+        ? {
+            interact: components as IInteractFn,
+          }
+        : { ...components };
     if (process.env.DATABOX_RUN_LATER) return;
 
     const options: IDataboxRunOptions = readCommandLineArgs();
+
+    if (!options.connectionToCore) {
+      const serverHost = UlixeeConfig.load()?.serverHost ?? UlixeeConfig.global.serverHost;
+      if (serverHost) {
+        options.connectionToCore = { host: serverHost };
+      }
+    }
+
     this.run(options).catch(error => {
       // eslint-disable-next-line no-console
       console.error(`ERROR running databox: `, error);
