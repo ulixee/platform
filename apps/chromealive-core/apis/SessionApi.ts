95d27ea13c8735096726df21433fd2ac02826b49
Blake Byrnes <blakebyrnes@gmail.com>
2022-02-11 17:06:53 -0500

    fix(timetravel): catch canceled queued loads

--- apps/chromealive-core/apis/SessionApi.ts
+++ apps/chromealive-core/apis/SessionApi.ts
@@ -3,6 +3,7 @@ import ISessionApi, {
   ISessionApiStatics,
   ISessionResumeArgs,
 } from '@ulixee/apps-chromealive-interfaces/apis/ISessionApi';
+import { CanceledPromiseError } from '@ulixee/commons/interfaces/IPendingWaitEvent';
 import SessionObserver from '../lib/SessionObserver';
 import ChromeAliveCore from '../index';
 
@@ -34,7 +35,14 @@ export default class SessionApi {
     timelineOffsetPercent: number;
   }> {
     const sessionObserver = getObserver(args);
-    return await sessionObserver.timetravel(args.percentOffset, args.step);
+    try {
+      return await sessionObserver.timetravel(args.percentOffset, args.step);
+    } catch (err) {
+      if (err instanceof CanceledPromiseError) {
+        return { timelineOffsetPercent: 100 };
+      }
+      throw err;
+    }
   }
 
   static step(args: IHeroSessionArgs): void {
