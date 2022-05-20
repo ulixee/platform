57d16e5ba4499657627e9d7a8c2221cb12b67094
Blake Byrnes <blakebyrnes@gmail.com>
2022-05-19 20:31:27 -0400

    chore: move commons out

--- commons/lib/VersionUtils.ts
+++
@@ -1,8 +0,0 @@
-import * as semver from 'semver'
-
-export function isSemverSatisfied(version: string, isSatisfiedByVersion: string): boolean {
-  return semver.satisfies(isSatisfiedByVersion, `~${version}`, { includePrerelease: true });
-}
-export function nextVersion(version :string): string {
-  return semver.inc(version, 'patch')
-}
