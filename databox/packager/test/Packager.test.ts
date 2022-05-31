a75162ac5cae234487e96bea1ff5b52f27a931f7
Blake Byrnes <blakebyrnes@gmail.com>
2022-05-31 14:57:15 -0400

    fix: packager tests

--- databox/packager/test/Packager.test.ts
+++ databox/packager/test/Packager.test.ts
@@ -1,9 +1,12 @@
 import DatabasePackager from '../index';
 import * as Fs from 'fs/promises';
+import * as Path from 'path';
 
 test('it should generate a relative script entrypoint', async () => {
   const packager = new DatabasePackager(`${__dirname}/assets/typescript/src/index.ts`);
-  expect(packager.outputPath).toBe(`${__dirname}/assets/typescript/src/.databox/index`);
+  expect(packager.outputPath).toBe(
+    Path.resolve(`${__dirname}/assets/typescript/src/.databox/index`),
+  );
   await packager.build();
   expect(packager.package.script).toBe(
     await Fs.readFile(packager.outputPath + '/databox.js', 'utf8'),
@@ -14,8 +17,9 @@ test('it should generate a relative script entrypoint', async () => {
 
   await expect(Fs.readFile(packager.outputPath + '/manifest.json', 'utf8')).resolves.toBeTruthy();
 
+  const sl = Path.sep;
   expect(packager.package.manifest).toEqual({
-    scriptEntrypoint: 'packager/test/assets/typescript/src/index.ts',
+    scriptEntrypoint: Path.join(`packager`, `test`, `assets`, `typescript`, `src`, `index.ts`),
     scriptRollupHash: expect.any(String),
     databoxModule: '@ulixee/databox-for-hero',
     databoxModuleVersion: require('../package.json').version,
