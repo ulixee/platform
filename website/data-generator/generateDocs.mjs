41c95fd31483229b73739ad17c16505112e377b8
Caleb Clark <calebjclark@gmail.com>
2022-06-08 16:08:24 -0500

    fix: updated website data generator to get ready for stream

--- website/data-generator/generateDocs.mjs
+++ website/data-generator/generateDocs.mjs
@@ -1,69 +1,36 @@
-import Fs from 'fs';
 import Path from 'path';
 import Url from 'url';
-import { paramCase } from "param-case";
-import MarkdownConverter from './MarkdownConverter.mjs';
+import { ensureIndexFile, saveToWebsite, walkDirectory } from './lib/DocsUtils.mjs';
+
 
 const __dirname = Path.dirname(Url.fileURLToPath(import.meta.url));
 const rootDir = Path.resolve(__dirname, '../../');
-const websiteDir = Path.join(rootDir, 'website');
 
 // const basePath = `${rootDir}/hero/docs`;
 // await saveToWebsite(basePath, `${basePath}/main/Overview/Configuration.md`, 'hero');
 
 {
-  const basePath = `${rootDir}/hero/docs`;
-  walkDirectory(basePath, async filePath => {
-    await saveToWebsite(basePath, filePath, 'hero');
+  const mdDocsRootPath = `${rootDir}/hero/docs`;
+  walkDirectory(mdDocsRootPath, async filePath => {
+    await saveToWebsite(mdDocsRootPath, filePath, 'hero');
   });
+  ensureIndexFile(mdDocsRootPath, 'hero', 'main');
 }
 
 {
-  const basePath = `${rootDir}/databox/docs`;
-  walkDirectory(basePath, async filePath => {
-    await saveToWebsite(basePath, filePath, 'databox');
+  const mdDocsRootPath = `${rootDir}/databox/docs`;
+  walkDirectory(mdDocsRootPath, async filePath => {
+    await saveToWebsite(mdDocsRootPath, filePath, 'databox');
   });
+  ensureIndexFile(mdDocsRootPath, 'databox');
 }
 
 {
-  const basePath = `${rootDir}/server/docs`;
-  walkDirectory(basePath, async filePath => {
-    await saveToWebsite(basePath, filePath, 'server');
+  const mdDocsRootPath = `${rootDir}/server/docs`;
+  walkDirectory(mdDocsRootPath, async filePath => {
+    await saveToWebsite(mdDocsRootPath, filePath, 'server');
   });
+  ensureIndexFile(mdDocsRootPath, 'server');
 }
 
-async function saveToWebsite(basePath, filePath, toolKey) {
-  const relativePath = Path.relative(basePath, filePath);
-  const converter = new MarkdownConverter(filePath);
-  const content = await converter.run();
-  const jsonRelativePath = convertToSlugPath(relativePath.replace(/\.md$/, '')) + '.json';
-  const jsonAbsolutePath = Path.join(websiteDir, `public/data/docs/${toolKey}/${jsonRelativePath}`);
-  const currentDir = Path.dirname(jsonAbsolutePath);
-  const data = {
-    content,
-    title: converter.title,
-    subtitles: converter.subtitles,
-  }
-  Fs.mkdirSync(currentDir, { recursive: true });
-  Fs.writeFileSync(jsonAbsolutePath, JSON.stringify(data, null, 2));
-  console.log('SAVED: ', jsonRelativePath);
-}
-
-function walkDirectory(directory, onFileFn) {
-  const fileNames = Fs.readdirSync(directory);
-  for (const fileName of fileNames) {
-    const filePath = `${directory}/${fileName}`;
-    const isDirectory = Fs.statSync(filePath).isDirectory();
-    if (isDirectory) {
-      process.nextTick(() => {
-        walkDirectory(filePath, onFileFn);
-      });
-    } else if (fileName.match(/\.md$/)) {
-      onFileFn(filePath)
-    }
-  }
-}
-
-function convertToSlugPath(path) {
-  return path.split('/').map(x => paramCase(x)).join('/');
 }
---
+++ website/data-generator/lib/DocsUtils.mjs
@@ -0,0 +1,55 @@
+import Fs from 'fs';
+import Path from 'path';
+import Url from 'url';
+import { paramCase } from "param-case";
+import MarkdownConverter from './MarkdownConverter.mjs';
+
+const __dirname = Path.dirname(Url.fileURLToPath(import.meta.url));
+const rootDir = Path.resolve(__dirname, '../../../');
+const websiteDir = Path.join(rootDir, 'website');
+
+export function ensureIndexFile(originalBasePath, toolKey, relativeWebsiteDir) {
+  relativeWebsiteDir = relativeWebsiteDir ? `${toolKey}/${relativeWebsiteDir}` : toolKey;
+  const originalPath = Path.join(originalBasePath, `${relativeWebsiteDir}/Index.md`);
+  const copyFromPath = Path.join(websiteDir, `public/data/docs/${relativeWebsiteDir}/Overview/Introduction.json`);
+  const copyToPath = Path.join(websiteDir, `public/data/docs/${relativeWebsiteDir}/index.json`);
+  if (Fs.existsSync(originalPath)) return;
+  if (!Fs.existsSync(copyFromPath)) return;
+  Fs.copyFileSync(copyFromPath, copyToPath);
+}
+
+export async function saveToWebsite(mdDocsRootPath, absoluteFilePath, toolKey) {
+  const relativePath = Path.relative(mdDocsRootPath, absoluteFilePath);
+  const converter = new MarkdownConverter(absoluteFilePath);
+  const content = await converter.run();
+  const jsonRelativePath = convertToSlugPath(relativePath.replace(/\.md$/, '')) + '.json';
+  const jsonAbsolutePath = Path.join(websiteDir, `public/data/docs/${toolKey}/${jsonRelativePath}`);
+  const currentDir = Path.dirname(jsonAbsolutePath);
+  const data = {
+    content,
+    title: converter.title,
+    subtitles: converter.subtitles,
+  }
+  Fs.mkdirSync(currentDir, { recursive: true });
+  Fs.writeFileSync(jsonAbsolutePath, JSON.stringify(data, null, 2));
+  console.log(`SAVED ${toolKey}/${jsonRelativePath}`);
+}
+
+export function walkDirectory(directory, onFileFn) {
+  const fileNames = Fs.readdirSync(directory);
+  for (const fileName of fileNames) {
+    const filePath = `${directory}/${fileName}`;
+    const isDirectory = Fs.statSync(filePath).isDirectory();
+    if (isDirectory) {
+      process.nextTick(() => {
+        walkDirectory(filePath, onFileFn);
+      });
+    } else if (fileName.match(/\.md$/)) {
+      onFileFn(filePath)
+    }
+  }
+}
+
+export function convertToSlugPath(path) {
+  return path.split('/').map(x => paramCase(x)).join('/');
+}
