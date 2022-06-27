e35606c2de1b7bf8605c06233a9fee0bcfa61074
Blake Byrnes <blakebyrnes@gmail.com>
2022-06-27 11:31:47 -0400

    fix(playground): server not shutting down

---
+++ playgrounds/hero/examples/example.org.ts
@@ -0,0 +1,35 @@
+import Hero from '@ulixee/hero-playground';
+import { LocationTrigger } from '@unblocked-web/specifications/agent/browser/Location';
+
+// NOTE: You need to start a Ulixee Server to run this example
+async function run() {
+  const hero = new Hero();
+  await hero.goto('https://example.org/');
+  await hero.waitForPaintingStable();
+
+  console.log('\n-- PRINTING location.href ---------');
+  console.log(await hero.url);
+
+  const outerHtml = await hero.document.documentElement.outerHTML;
+  const linkElement = hero.document.querySelector('a');
+
+  console.log('-- PRINTING outerHTML of link ---------------');
+  console.log(outerHtml);
+  console.log('OUTPUT from https://example.org', {
+    outerHtml,
+    title: await hero.document.title,
+    intro: await hero.document.querySelector('p').textContent,
+    linkTag: await linkElement.outerHTML,
+  });
+  console.log('-------------------------------------');
+
+  await linkElement.$click();
+  await hero.waitForLocation(LocationTrigger.change);
+  console.log('NEW LOCATION: ', await hero.document.location.href);
+  console.log('-------------------------------------');
+
+  await hero.close();
+}
+
+run().catch(error => console.log(error));
+
