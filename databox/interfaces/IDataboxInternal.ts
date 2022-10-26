ae94176c237c8c729b426c783e3918ed460c6c0e
Blake Byrnes <blakebyrnes@gmail.com>
2022-10-26 12:31:31 -0400

    fix: lint issue

--- databox/interfaces/IDataboxInternal.ts
+++ databox/interfaces/IDataboxInternal.ts
@@ -1,3 +1,4 @@
 import IDataboxSchema from './IDataboxSchema';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default interface IDataboxInternal<ISchema extends IDataboxSchema> {}
